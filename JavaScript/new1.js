5802a60789c45cb891f73d4066f7b39291d417f4
noteLearn <makertuwem@outlook.com>
2017-10-09 13:32:47 +0800

    add changeimg.html

--- JavaScript/new1.js
+++ JavaScript/new1.js
@@ -1,13 +1,13 @@
-x=document.getElementById("intro");
+/*x=document.getElementById("intro");
 document.write('<p>id="intro"的段落是：' + x.innerHTML + '</p>');
 y=document.getElementById("Tijn").innerHTML="Tijn";
-document.write(y);
+document.write(y);*/
 
 
 function changeimg(id){
-document.getElementById("butong").src="file:///H:/Users/TuWen/Download/IDM/Photos/26/64373501_p0_master1200.jpg"
+document.getElementById("butong").src="file:///D:/GoogleDrive/GAME/PSP/SAVE/%E6%80%AA%E7%89%A9%E7%8C%8E%E4%BA%BAP3/ULJM05800/PIC1.PNG"
 };
-document.getElementById("butong").onclick=function(){
+document.getElementById("p1").onclick=function(){
     changeimg();
     var para=document.createElement("p");
     var node=document.createTextNode("很遗憾只能更改一次，如需再次体验请按<F5>");
@@ -16,4 +16,3 @@ document.getElementById("butong").onclick=function(){
     var child=document.getElementById("p2");
     element.replaceChild(para,child)
 };
-
