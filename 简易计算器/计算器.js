01edd1f25b7716b1e913cd6c65be828fd3adc6b8
noteLearn <makertuwem@outlook.com>
2017-10-11 19:21:37 +0800

    开始计算器界面美化

--- 简易计算器/计算器.js
+++ 简易计算器/计算器.js
@@ -1,39 +1,73 @@
 var result_1;
+var result_2;
+var re;
+
 
 //加法
 function add(){
     var a=getFirst();
     var b=getTwice();
-    var re=Number(a)+Number(b);
+    re=Number(a)+Number(b);
     senResult(re);
 }
 
 function subtract(){
     var a=getFirst();
     var b=getTwice();
-    var re=a-b;
+    re=a-b;
     senResult(re);
 }
 
 function ride(){
     var a=getFirst();
     var b=getTwice();
-    var re=a*b;
+    re=a*b;
     senResult(re);
 }
 
 function devide(){
     var a=getFirst();
     var b=getTwice();
-    var re=a/b;
+    re=a/b;
     senResult(re);
 }
 
+function addB() {
+    var c= getSecond();
+    reB = Number(re) + Number(c);
+    senResultB(reB);
+}
+
+function subtractB(){
+    var c= getSecond();
+    reB = re - c;
+    senResultB(reB);
+}
+
+function rideB() {
+    var c= getSecond();
+    reB = re * c;
+    senResultB(reB);
+}
+
+function devideB() {
+    var c= getSecond();
+    reB = re / c;
+    senResultB(reB);
+}
+
+
 //输出p值
 function senResult(result_1){
-    var num=document.getElementById("result");
+    var num=document.getElementById("resultA");
     num.innerHTML=result_1;
 }
+
+function senResultB(result_2){
+    var numB=document.getElementById("resultB");
+    numB.innerHTML = result_2;
+}
+
 
 //获取输入值
 function getFirst(){
@@ -46,6 +80,13 @@ function getTwice(){
     return twice;
 }
 
+function getSecond(){
+    var second=document.getElementById("second").value;
+    return second;
+}
 
 
+function changeColorFir() {
+    document.getElementById("first").stytle="background-color:#FFFFFFF";
+}
 
