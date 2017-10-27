3244f0e51db3b8d5f926bec80555c30179bb5c3c
noteLearn <makertuwem@outlook.com>
2017-10-27 14:44:57 +0800

    start tmall header

--- Tmall/tmall.com_clone.js
+++ Tmall/tmall.com_clone.js
@@ -1,1 +1,21 @@
-
+function treasuieSubmenuover() {
+        var a=document.getElementById("nav-treasuie-submenu");
+    a.setAttribute("style","display:block;z-index:99999;border-color:#eee;");
+    }
+    function treasuieSubmenuout() {
+        document.getElementById("nav-treasuie-submenu").setAttribute("style","display:none;z-index:99998;");
+    }
+    function bookSubmenuover() {
+        var a=document.getElementById("nav-book-submenu");
+    a.setAttribute("style","display:block;z-index:99999;border-color:#eee;");
+    }
+    function bookSubmenuout() {
+        document.getElementById("nav-book-submenu").setAttribute("style","display:none;z-index:99998;");
+    }
+    function businessSubmenuover() {
+        var a=document.getElementById("nav-business-submenu");
+    a.setAttribute("style","display:block;z-index:99999;border-color:#eee;");
+    }
+    function businessSubmenuout() {
+        document.getElementById("nav-business-submenu").setAttribute("style","display:none;z-index:99998;");
+    }
