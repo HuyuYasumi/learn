e6768549552b8ace5c658f8bae3bc20354cf009c
noteLearn <makertuwem@outlook.com>
2017-10-29 13:00:28 +0800

    upgrade ignore

--- Tmall/tmall.com_clone.js
+++ Tmall/tmall.com_clone.js
@@ -1,20 +1,20 @@
 function treasuieSubmenuover() {
     var a=document.getElementById("nav-treasuie-submenu");
-a.setAttribute("style","display:block;z-index:3;border-color:#eee;");
+    a.setAttribute("style","display:block;z-index:3;border-color:#eee;");
 }
 function treasuieSubmenuout() {
     document.getElementById("nav-treasuie-submenu").setAttribute("style","display:none;z-index:2;");
 }
 function bookSubmenuover() {
     var a=document.getElementById("nav-book-submenu");
-a.setAttribute("style","display:block;z-index:3;border-color:#eee;");
+    a.setAttribute("style","display:block;z-index:3;border-color:#eee;");
 }
 function bookSubmenuout() {
     document.getElementById("nav-book-submenu").setAttribute("style","display:none;z-index:2;");
 }
 function businessSubmenuover() {
     var a=document.getElementById("nav-business-submenu");
-a.setAttribute("style","display:block;z-index:3;border-color:#eee;");
+    a.setAttribute("style","display:block;z-index:3;border-color:#eee;");
 }
 function businessSubmenuout() {
     document.getElementById("nav-business-submenu").setAttribute("style","display:none;z-index:2;");
