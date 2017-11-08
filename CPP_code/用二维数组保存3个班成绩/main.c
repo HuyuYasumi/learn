737626e8c0ac7e8ed30fe8e9e2e4b3062e28f0d3
noteLearn <makertuwem@outlook.com>
2017-11-08 23:52:41 +0800

    CPP CPP_code/用二维数组保存3个班成绩/main.c

---
+++ CPP_code/用二维数组保存3个班成绩/main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define CLASSES 3
+#define GRADES 5
+
+int grade[CLASSES][GRADES];
+
+void enter_grades(void);
+int get_grades(int num);
+void disp_grades(int g[ ][GRADES]);
+
+int main()
+{
+    char ch,str[80];
+    for(;;) {
+        do {
+            printf("(E)nter grades \n");
+            printf("(R)eport grades \n");
+            printf("(Q)uit \n");
+            gets(str);
+            ch=toupper(*str);
+        }while(ch != 'E' && ch != 'R' && ch != 'Q');
+
+        switch(ch) {
+            case 'E':
+                enter_grades();
+                break;
+            case 'R':
+                disp_grades(grade);
+                break;
+            case 'Q':
+                exit(0);
+        }
+    }
+    return 0;
+}
+
+void enter_grades(void)
+{
+    int t,i;
+
+    for(t=0; t<CLASSES; t++) {
+        printf("Class #%d: \n", t+1);
+        for(i=0; i<GRADES; ++i) grade[t][i]=get_grade(i);
+    }
+}
+
+int get_grade(int num)
+{
+    char s[80];
+
+    printf("Enter grade for student #%d: \n",num+1);
+    gets(s);
+    return(atoi(s));
+}
+
+void disp_grades(int g[][GRADES])
+{
+    int t,i;
+
+    for(t=0; t<CLASSES; ++t) {
+        printf("Class #%d: \n", t+1);
+        for(i=0; i<GRADES; ++i) printf("Student #%d is %d \n",i+1,g[t][i]);
+    }
+}
