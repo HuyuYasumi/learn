f5fe0a943ece01d0b174791d641f94654c9090f7
noteLearn <makertuwem@outlook.com>
2017-12-21 18:06:21 +0800

    modified:   简明Python教程/ABoP_20171221_Linux标准zip命令_将文件或文件夹备份为zip文件.py #01

--- 简明Python教程/ABoP_20171221_Linux标准zip命令_将文件或文件夹备份为zip文件.py
+++ 简明Python教程/ABoP_20171221_Linux标准zip命令_将文件或文件夹备份为zip文件.py
@@ -2,18 +2,18 @@ import os
 import time
 
 #需要备份的文件或文件夹
-source=["F:\learn\简明Python教程\Python_temp\新建文件夹"]
+source = ["F:\learn\简明Python教程\Python_temp\新建文件夹", "F:\learn\简明Python教程\Python_temp\新建文件夹2"]
 #主备份文件夹
-target_dir='F:\learn\简明Python教程\Python_temp\新建文件夹 (2)'
+target_dir = 'F:\learn\简明Python教程\Python_temp'
 
 #备份目录及文件名
-target=target_dir+os.sep+time.strftime('%Y%m%d%H%M%S')+'.zip'
+target = target_dir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
 #如不存在目标目录，则创建
 if not os.path.exists(target_dir):
     os.mkdir(target_dir)
 
 #使用zip命令将文件打包成zip格式文件
-zip_command='zip -r {0} {1}'.format(target, ' '.join(source))
+zip_command = 'zip -r {0} {1}'.format(target, ' '.join(source))
 
 #运行备份
 print('Zip command is:')
