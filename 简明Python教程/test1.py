8dda15ed36ecaef061e3a38483d9a848451d640b
noteLearn <makertuwem@outlook.com>
2017-12-23 01:04:20 +0800

    使用类变量与类方法 modified:   test1.py

--- 简明Python教程/test1.py
+++ 简明Python教程/test1.py
@@ -1,56 +1,17 @@
-import sys
-age=17
-name='Ore'
-print('{} was {} years old.'.format(name, age), end=' ')
-print('{0:_^11}'.format(name), 1)
+#-*-coding:utf-8-*-
 
-running=True
+class Student():
+    '''使用Student.getcount()获取当前学生数量'''
+    count = 0
+    def __init__(self, name):
+        self.name = name
+        Student.count += 1
 
-def say(message, times=1):
-    print(message*times)
+    @classmethod
+    def getcount(self):
+        print(Student.count)
+        return Student.count
 
-# while running:
-    # number=int(input('Enter an integer:'))
-    # if age == number:
-        # say('Congratulations, you guessed it.\n')
-        # running=False
-    # elif age < number:
-        # say(number, message='No, it is a little highter than that.\n')
-    # else :
-        # say('No, it is a little lower than that.\n', number)
-# else:
-    # print('The while loop is over.')
-
-# maxint=int(input('Enter an integer:'))
-# for i in range(1, maxint):
-    # print(i)
-# else:
-    # print('The for loop is over')
-
-# print(dir(age))
-# print(dir(name))
-# print(dir(format))
-# print(dir(say))
-# print(dir(sys))
-
-if sys.__name__ == '__main__':
-    print('sys')
-if __name__ == '__main__':
-    print('this')
-
-class Person:
-    def __init__(self, name='Hello'):
-        self.name=name
-    def say_hi(self):
-        print('Hellw, how are you?', self.name)
-    def say_hello(self):
-        print('Hi, how are you?', self.name)
-    name='Hi'
-    def say_mother(self):
-        print(self.name, 'How are you')
-
-p = Person()
-p.say_hi()
-p.say_hello()
-print(p.name)
-p.say_mother()
+Marry = Student('Marry')
+Jack = Student('Jack')
+Student.getcount()
