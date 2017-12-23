563e507f3845fcf13aac19ced57e2c89de10a1c4
noteLearn <makertuwem@outlook.com>
2017-12-23 13:40:05 +0800

    查找序列最小值最大值 modified:   简明Python教程/test1.py

--- 简明Python教程/test1.py
+++ 简明Python教程/test1.py
@@ -1,17 +1,30 @@
 #-*-coding:utf-8-*-
 
-class Student():
-    '''使用Student.getcount()获取当前学生数量'''
-    count = 0
-    def __init__(self, name):
-        self.name = name
-        Student.count += 1
+def findMinAndMax(l):
+    if len(l) == 0:
+        return (None, None)
+    else:
+        minint = l[0]
+        maxint = l[0]
+        for y in range(1, len(l)):
+            if maxint < l[y]:
+                maxint = l[y]
+            if minint > l[y]:
+                minint = l[y]
+        return (minint, maxint)
 
-    @classmethod
-    def getcount(self):
-        print(Student.count)
-        return Student.count
-
-Marry = Student('Marry')
-Jack = Student('Jack')
-Student.getcount()
+# 测试
+if findMinAndMax([]) != (None, None):
+    print('测试失败!#1')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!#2')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!#3')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!#4')
+elif findMinAndMax([4, 2, 7]) != (2, 7):
+    print('测试失败!#5')
+elif findMinAndMax([3]) != (3, 3):
+    print('测试失败!#6')
+else:
+    print('测试成功!')
