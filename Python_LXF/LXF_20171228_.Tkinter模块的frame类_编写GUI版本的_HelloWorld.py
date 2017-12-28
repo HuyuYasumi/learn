09ed3e9417fc5487e2f938b2a656b71f16d40846
noteLearn <makertuwem@outlook.com>
2017-12-28 19:26:42 +0800

    Optimized_#01__LXF_20171228_.Tkinter模块的frame类_编写GUI版本的_HelloWorld.py

--- Python_LXF/LXF_20171228_.Tkinter模块的frame类_编写GUI版本的_HelloWorld.py
+++ Python_LXF/LXF_20171228_.Tkinter模块的frame类_编写GUI版本的_HelloWorld.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox as messagebox
 
 class Application(Frame):
     def __init__(self, master = None):
@@ -9,8 +10,14 @@ class Application(Frame):
     def createWidgets(self):
         self.helloLabel = Label(self, text = 'Hello, World!')
         self.helloLabel.pack()
-        self.quitButton = Button(self, text = 'Quit', command = self.quit)
-        self.quitButton.pack()
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text = 'Hello', command = self.hello)
+        self.alertButton.pack()
+
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
 
 app = Application()
 app.master.title('Hello World')
