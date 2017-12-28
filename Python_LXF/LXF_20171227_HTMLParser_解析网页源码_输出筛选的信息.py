851dcc4d4ca0c9b09316532c02cc168d0e955046
noteLearn <makertuwem@outlook.com>
2017-12-28 18:48:22 +0800

    Optimized_#01__LXF_20171227_HTMLParser_解析网页源码_输出筛选的信息.py

--- Python_LXF/LXF_20171227_HTMLParser_解析网页源码_输出筛选的信息.py
+++ Python_LXF/LXF_20171227_HTMLParser_解析网页源码_输出筛选的信息.py
@@ -10,42 +10,51 @@ class MyHTMLParser(HTMLParser):
                 if r'list-recent-events menu' in attr[1]:
                     #print([x for x in attrs])
                     self.flag = True
-        if self.flag == True and tag == 'a':
-            self.count = self.count + 1
-            self.name = True
-        if self.flag == True and tag == 'time':
-            self.time = True
-        if self.flag == True and tag == 'span':
-            self.location = True
+        if self.flag:
+            if tag == 'a':
+                self.count = self.count + 1
+                self.name = True
+            elif tag == 'time':
+                self.time = True
+            elif tag == 'span':
+                self.location = True
 
     def handle_data(self, data):
-        if self.name == True:
-            self.fdata['name%s' % self.count] = data
-            self.name = False
-        if self.time == True:
-            self.fdata['time%s' % self.count] = data
-            self.time = False
-        if self.location == True:
-            self.fdata['location%s' % self.count] = data
-            self.location = False
+        if data:
+            if self.name:
+                self.fdata['name%s' % self.count] = data
+                self.name = False
+            if self.time:
+                self.fdata['time%s' % self.count] = data
+                self.time = False
+            if self.location:
+                self.fdata['location%s' % self.count] = data
+                self.location = False
+        else:
+            if self.name:
+                self.fdata['name%s' % self.count] = none
+                self.name = False
+            if self.time:
+                self.fdata['time%s' % self.count] = none
+                self.time = False
+            if self.location:
+                self.fdata['location%s' % self.count] = none
+                self.location = False
 
     def handle_endtag(self, tag):
-        if self.flag == True and tag == 'ul':
+        if self.flag and tag == 'ul':
             self.flag = False
 
     def show_events(self):
         for i in range(1, self.count+1):
             print('Event #%s:' % i)
-            if self.fdata['name%s' % i]:
-                print('Name:',self.fdata['name%s' % i])
-            if self.fdata['time%s' % i]:
-                print('Time:', self.fdata['time%s' % i])
-            if self.fdata['location%s' % i]:
-                print('Location:', self.fdata['location%s' % i], '\n')
+            print('Name:',self.fdata['name%s' % i])
+            print('Time:', self.fdata['time%s' % i])
+            print('Location:', self.fdata['location%s' % i], '\n')
         return self.fdata
 
 parser = MyHTMLParser()
-with request.urlopen('https://www.python.org/events/python-events/') as f:
+with request.urlopen('https://www.python.org/events/python-events/', timeout=4) as f:
     parser.feed(f.read().decode('utf-8'))
 parser.show_events()
 
