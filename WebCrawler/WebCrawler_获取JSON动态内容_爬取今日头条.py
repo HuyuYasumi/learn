14ea768fafebf73f6ad4715f8bd2c86656472052
noteLearn <makertuwem@outlook.com>
2018-01-15 09:46:36 +0800

    New  WebCrawler/WebCrawler_获取JSON动态内容_爬取今日头条.py

---
+++ WebCrawler/WebCrawler_获取JSON动态内容_爬取今日头条.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+import requests, json
+
+url = 'http://www.toutiao.com/api/pc/focus/'
+wbdata = requests.get(url).text
+
+data = json.loads(wbdata)
+news = data['data']['pc_feed_focus']
+
+for n in news:
+    title = n['title']
+    img_url = n['image_url']
+    url = n['media_url']
+    print(url, title, img_url)
