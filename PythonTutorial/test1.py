1cb58fde0d4cb6e9e4610e00c1bf742e7f19d81d
ubuntuNatsu <makertuwem@outlook.com>
2018-01-28 12:19:41 +0800

    Modified: PythonTutorial/IP_装饰器.py

--- PythonTutorial/test1.py
+++ PythonTutorial/test1.py
@@ -1,30 +1,29 @@
-#-*-coding:utf-8-*-
+from functools import wraps
 
-def findMinAndMax(l):
-    if len(l) == 0:
-        return (None, None)
-    else:
-        minint = l[0]
-        maxint = l[0]
-        for y in range(1, len(l)):
-            if maxint < l[y]:
-                maxint = l[y]
-            if minint > l[y]:
-                minint = l[y]
-        return (minint, maxint)
+class logit(object):
+    def __init__(self, logfile = 'out.log'):
+        self.logfile = logfile
 
-# 测试
-if findMinAndMax([]) != (None, None):
-    print('测试失败!#1')
-elif findMinAndMax([7]) != (7, 7):
-    print('测试失败!#2')
-elif findMinAndMax([7, 1]) != (1, 7):
-    print('测试失败!#3')
-elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
-    print('测试失败!#4')
-elif findMinAndMax([4, 2, 7]) != (2, 7):
-    print('测试失败!#5')
-elif findMinAndMax([3]) != (3, 3):
-    print('测试失败!#6')
-else:
-    print('测试成功!')
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped_function(*args, **kwargs):
+            log_string = func.__name__ + ' was called'
+            print(log_string)
+            print(self.logfile, 'now was write')
+            self.notify()
+            return func(*args, **kwargs)
+        return wrapped_function()
+
+    def notify(self):
+        pass
+
+@logit()
+def myfunc():
+    pass
+
+class email_logit(logit):
+    def __init__(self, email='admmin@myproject.com', *args, **kwargs):
+        self.email = email
+
+    def notify(self):
+        pass
