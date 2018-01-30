7963e08f401cfdf1186124d37c1946af08580e07
ubuntuNatsu <makertuwem@outlook.com>
2018-01-30 22:29:19 +0800

    Modified: PythonTutorial/test1.py

--- PythonTutorial/test1.py
+++ PythonTutorial/test1.py
@@ -1,29 +1,82 @@
-from functools import wraps
+# coding: utf8
+import sys, mysql.connector
 
-class logit(object):
-    def __init__(self, logfile = 'out.log'):
-        self.logfile = logfile
+class TransferMoney():
+    def __init__(self, conn):
+        self.conn = conn
 
-    def __call__(self, func):
-        @wraps(func)
-        def wrapped_function(*args, **kwargs):
-            log_string = func.__name__ + ' was called'
-            print(log_string)
-            print(self.logfile, 'now was write')
-            self.notify()
-            return func(*args, **kwargs)
-        return wrapped_function()
+    def check_acct_available(self, acctid):
+        cursor = self.conn.cursor()
+        try:
+            sql = 'select * from account where acctid=%s' % acctid
+            cursor.execute(sql)
+            print('check_acct_available: ' + sql)
+            rs = cursor.fetchall()
+            if len(rs) != 1:
+                raise Exception('帐号%s不存在' % acctid)
+        finally:
+            cursor.close()
 
-    def notify(self):
-        pass
+    def has_enough_money(self, acctid, money):
+        cursor = self.conn.cursor()
+        try:
+            sql = 'select * from account where acctid=%s and money>%s' % (acctid, money)
+            cursor.execute(sql)
+            print('has_enough_money: ' + sql)
+            rs = cursor.fetchall()
+            if len(rs) != 1:
+                raise Exception('帐号%s没有足够的钱' % acctid)
+        finally:
+            cursor.close()
 
-@logit()
-def myfunc():
-    pass
+    def reduce_money(self, acctid, money):
+        cursor = self.conn.cursor()
+        try:
+            sql = 'update account set money=money-%s where acctid=%s' % (money, acctid)
+            cursor.execute(sql)
+            print('reduce_money: ' + sql)
+            if cursor.rowcount != 1:
+                raise Exception('帐号%s减款失败' % acctid)
+        finally:
+            cursor.close()
 
-class email_logit(logit):
-    def __init__(self, email='admmin@myproject.com', *args, **kwargs):
-        self.email = email
+    def add_money(self, acctid, money):
+        cursor = self.conn.cursor()
+        try:
+            sql = 'update account set money=money+%s where acctid=%s' % (money, acctid)
+            cursor.execute(sql)
+            print('add_money: ' + sql)
+            if cursor.rowcount != 1:
+                raise Exception('帐号%s加款失败' % acctid)
+        finally:
+            cursor.close()
 
-    def notify(self):
-        pass
+
+    def transfer(self, source_acctid, target_acctid, money):
+        try:
+            self.check_acct_available(source_acctid)
+            self.check_acct_available(target_acctid)
+            self.has_enough_money(source_acctid, money)
+            self.reduce_money(source_acctid, money)
+            self.add_money(target_acctid, money)
+            self.conn.commit()
+        except Exception as e:
+            self.conn.rollback()
+            raise e
+
+if __name__ == '__main__':
+    source_acctid = sys.argv[1]
+    target_acctid = sys.argv[2]
+    money = sys.argv[3]
+
+    conn = mysql.connector.connect(user = 'test',
+                                   password = 'test1234',
+                                   database = 'test')
+    tr_money = TransferMoney(conn)
+
+    try:
+        tr_money.transfer(source_acctid, target_acctid, money)
+    except Exception as e:
+        print('出现问题: '+str(e))
+    finally:
+        conn.close()
